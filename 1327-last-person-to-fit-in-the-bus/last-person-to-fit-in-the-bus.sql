bdf318893077259fc93adb2319ebf87408748122
Jahnavi <52441782+JahnaviiB@users.noreply.github.com>
2023-12-20 16:43:57 -0500

    Time: 2993 ms (20.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select
+person_name
+from Queue as q
+where 1000 >= (
+    select
+    sum(weight)
+    from Queue
+    where q.turn >= turn
+)
+order by turn desc
+limit 1
